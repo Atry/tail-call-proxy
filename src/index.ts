9ca2dd3d7bb462e4a468dfe066e7c89fb3d44923
Yang, Bo <pop.atry@gmail.com>
2023-01-12 18:25:38 -0800

    feat: initial implementation
    Added lazy and parasitic functions
    
    BREAKING CHANGE: Removed the myPackage function from the template

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,168 @@
-export const myPackage = (taco = ''): string => `${taco} from my package`;
+/* eslint-disable @typescript-eslint/ban-types */
+type ProxyTarget<T extends object> = {
+  tailCall?: () => T;
+  tail?: ProxyTarget<T>;
+  result?: T;
+  error?: any;
+};
+
+function getResult<T extends object>(target: ProxyTarget<T>): T {
+  if ('result' in target) {
+    return target['result'] as T;
+  } else if ('error' in target) {
+    throw target['error'];
+  } else {
+    run(target);
+    return getResult(target);
+  }
+}
+
+const TARGET = Symbol();
+
+let parasiticQueue: ProxyTarget<any>[] = [];
+
+let isRunning = false;
+
+function step<T extends object>(target: ProxyTarget<T>): void {
+  const { tailCall } = target;
+  let proxyOrResult: T;
+  if (tailCall !== undefined) {
+    try {
+      try {
+        proxyOrResult = tailCall();
+      } finally {
+        delete target.tailCall;
+      }
+    } catch (error) {
+      target.error = error;
+      return;
+    }
+    if (proxyOrResult == null) {
+      target.result = proxyOrResult;
+    } else {
+      const nonNullProxyOrResult: { [TARGET]?: ProxyTarget<T> } = proxyOrResult;
+      const { [TARGET]: tail } = nonNullProxyOrResult;
+      if (tail === undefined) {
+        target.result = proxyOrResult;
+      } else {
+        target.tail = tail;
+      }
+    }
+  }
+}
+
+function resolveWith<T extends object>(
+  headTarget: ProxyTarget<T>,
+  lastTarget: ProxyTarget<T>
+): void {
+  if ('error' in lastTarget) {
+    headTarget.error = lastTarget.error;
+  } else {
+    headTarget.result = lastTarget.result;
+  }
+}
+
+function resolve<T extends object>(headTarget: ProxyTarget<T>): void {
+  step(headTarget);
+  if (headTarget.tail !== undefined) {
+    let target = headTarget.tail;
+    delete headTarget.tail;
+    for (;;) {
+      step(target);
+      const { tail } = target;
+      const queue = parasiticQueue;
+      if (queue.length !== 0) {
+        parasiticQueue = [];
+        for (const parasiticTarget of queue) {
+          if (parasiticTarget !== tail) {
+            resolve(parasiticTarget);
+          }
+        }
+      }
+      if (tail !== undefined) {
+        target = tail;
+      } else {
+        resolveWith(headTarget, target);
+        return;
+      }
+    }
+  }
+}
+
+function run<T extends object>(target: ProxyTarget<T>): void {
+  if (isRunning) {
+    resolve(target);
+  }
+  isRunning = true;
+  try {
+    resolve(target);
+  } finally {
+    isRunning = false;
+  }
+}
+
+const LAZY_PROXY_HANDLER: ProxyHandler<ProxyTarget<object>> =
+  Object.fromEntries(
+    Object.entries(Object.getOwnPropertyDescriptors(Reflect)).map(
+      ([functionName, { value }]) => {
+        switch (functionName) {
+          case 'get':
+            return [
+              functionName,
+              (
+                target: ProxyTarget<object>,
+                propertyKey: string | symbol,
+                receiver: unknown
+              ) => {
+                switch (propertyKey) {
+                  case TARGET:
+                    return target;
+                  default: {
+                    const result = getResult(target);
+                    const property: unknown = Reflect.get(
+                      result,
+                      propertyKey,
+                      receiver
+                    );
+                    if (typeof property === 'function') {
+                      return function (this: any, ...argArray: any[]): any {
+                        Reflect.apply(
+                          property,
+                          this === receiver ? result : this,
+                          argArray
+                        );
+                      };
+                    } else {
+                      return property;
+                    }
+                  }
+                }
+              },
+            ];
+          default:
+            return [
+              functionName,
+              (target: ProxyTarget<object>, ...args: any) =>
+                (value as (target: any, ...args: any) => unknown)(
+                  getResult(target),
+                  ...args
+                ),
+            ];
+        }
+      }
+    )
+  );
+
+export function lazy<T extends object>(tailCall: () => T): T {
+  return new Proxy({ tailCall }, LAZY_PROXY_HANDLER) as T;
+}
+
+export function parasitic<T extends object>(tailCall: () => T): T {
+  const target: ProxyTarget<T> = { tailCall };
+  if (isRunning) {
+    parasiticQueue.push(target);
+    return new Proxy(target, LAZY_PROXY_HANDLER) as T;
+  } else {
+    return getResult(target);
+  }
+}
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,13 +1,89 @@
-import { myPackage } from '../src';
+import { parasitic, lazy } from '../src';
 
 describe('index', () => {
-  describe('myPackage', () => {
-    it('should return a string containing the message', () => {
-      const message = 'Hello';
+  describe('parasitic', () => {
+    it('should actually return a number from a number recursive function', () => {
+      function go(i: number): { x: number } {
+        if (i < 1000000) {
+          return parasitic(() => go(i + 1));
+        } else {
+          return { x: 42 };
+        }
+      }
+      expect(go(0).x).toBe(42);
+    });
 
-      const result = myPackage(message);
-
-      expect(result).toMatch(message);
+    it('should actually return numbers from nested number recursive functions', () => {
+      function fortyTwo(i: number): { x: number } {
+        if (i < 1000000) {
+          return parasitic(() => fortyTwo(i + 1));
+        } else {
+          return { x: 42 };
+        }
+      }
+      type List =
+        | { value: { x: number }; next: List }
+        | { value?: undefined; next?: undefined };
+      function list(i: number, accumulator: List): List {
+        if (i < 3) {
+          return parasitic(() =>
+            list(i + 1, { value: fortyTwo(0), next: accumulator })
+          );
+        } else {
+          return accumulator;
+        }
+      }
+      expect(list(0, {})?.value?.x).toBe(42);
+    });
+  });
+  describe('lazy', () => {
+    it('should not throw a stack overflow error from an asynchronous recursive functions', async () => {
+      async function go(i: number): Promise<number> {
+        if (i < 3) {
+          return lazy(() => go(i + 1));
+        } else {
+          return 42;
+        }
+      }
+      expect(await go(0)).toBe(42);
+    });
+    it('should execute aliases of a tail call proxy only once', () => {
+      let count = 0;
+      const buffer: { x: number }[] = [];
+      function fortyTwo(i: number): { x: number } {
+        if (i < 3) {
+          return lazy(() => fortyTwo(i + 1));
+        } else {
+          count++;
+          return { x: 42 };
+        }
+      }
+      function sideEffect(): { x: number } {
+        const result = fortyTwo(0);
+        buffer.push(result);
+        return result;
+      }
+      function tailCallSideEffect(): { x: number } {
+        return lazy(() => sideEffect());
+      }
+      type List =
+        | { value: { x: number }; next: List }
+        | { value?: undefined; next?: undefined };
+      function list(i: number, accumulator: List): List {
+        if (i < 2) {
+          return lazy(() =>
+            list(i + 1, { value: tailCallSideEffect(), next: accumulator })
+          );
+        } else {
+          return accumulator;
+        }
+      }
+      expect(list(0, {})).toMatchObject({
+        value: { x: 42 },
+        next: { value: { x: 42 }, next: {} },
+      });
+      expect(buffer).toMatchObject([{ x: 42 }, { x: 42 }]);
+      expect(count).toBe(2);
     });
   });
 });
