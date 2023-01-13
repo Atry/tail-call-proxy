8d328a0ad44fe0756d5946991fc4973c5ad6fba4
Yang, Bo <pop.atry@gmail.com>
2023-01-12 23:29:35 -0800

    docs: Add badges

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,30 @@
 
 /**
  * Delayed initialized objects that support tail-call optimization.
+ *
+ * [![npm package][npm-img]][npm-url]
+ * [![Build Status][build-img]][build-url]
+ * [![Downloads][downloads-img]][downloads-url]
+ * [![Issues][issues-img]][issues-url]
+ * [![Code Coverage][codecov-img]][codecov-url]
+ * [![Commitizen Friendly][commitizen-img]][commitizen-url]
+ * [![Semantic Release][semantic-release-img]][semantic-release-url]
+ *
+ * [build-img]:https://github.com/Atry/tail-call-proxy/actions/workflows/release.yml/badge.svg
+ * [build-url]:https://github.com/Atry/tail-call-proxy/actions/workflows/release.yml
+ * [downloads-img]:https://img.shields.io/npm/dt/tail-call-proxy
+ * [downloads-url]:https://www.npmtrends.com/tail-call-proxy
+ * [npm-img]:https://img.shields.io/npm/v/tail-call-proxy
+ * [npm-url]:https://www.npmjs.com/package/tail-call-proxy
+ * [issues-img]:https://img.shields.io/github/issues/Atry/tail-call-proxy
+ * [issues-url]:https://github.com/Atry/tail-call-proxy/issues
+ * [codecov-img]:https://codecov.io/gh/Atry/tail-call-proxy/branch/main/graph/badge.svg
+ * [codecov-url]:https://codecov.io/gh/Atry/tail-call-proxy
+ * [semantic-release-img]:https://img.shields.io/badge/%20%20%F0%9F%93%A6%F0%9F%9A%80-semantic--release-e10079.svg
+ * [semantic-release-url]:https://github.com/semantic-release/semantic-release
+ * [commitizen-img]:https://img.shields.io/badge/commitizen-friendly-brightgreen.svg
+ * [commitizen-url]:http://commitizen.github.io/cz-cli/
+ *
  * @module
  */
 
