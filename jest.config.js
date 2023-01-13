806887535d015df37e64c977b46a5d02fbbe6f0f
Yang, Bo <pop.atry@gmail.com>
2023-01-13 15:56:23 -0800

    build: configure ts-jest using `transform` instead of `global`

--- jest.config.js
+++ jest.config.js
@@ -3,18 +3,20 @@ const requireJSON5 = require('require-json5');
 const { compilerOptions } = requireJSON5('./tsconfig.json');
 
 module.exports = {
-  preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/test/**/*.spec.ts', '**/test/**/*.test.ts'],
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',
     '!<rootDir>/src/types/**/*.ts',
   ],
-  globals: {
-    'ts-jest': {
-      diagnostics: false,
-      isolatedModules: true,
-    },
+  transform: {
+    '^.+\\.m?tsx?$': [
+      'ts-jest',
+      {
+        diagnostics: false,
+        isolatedModules: true,
+      },
+    ],
   },
   roots: ['<rootDir>'],
   modulePaths: [compilerOptions.baseUrl],
